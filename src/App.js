810fa0a534f85ec0a05f7a7684170c22f526c6e4
tmrk <tamas.marki@gmail.com>
2022-06-21 15:26:53 +0200

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
+import { ReactComponent as Checkmark } from './assets/check.svg';
+import { ReactComponent as Refresh } from './assets/refresh.svg';
+import { ReactComponent as SelectAll } from './assets/select_all.svg';
+import { ReactComponent as Deselect } from './assets/deselect.svg';
+import { ReactComponent as FileDownload } from './assets/file_download.svg';
 
 const jsonAddress = 'https://gist.githubusercontent.com/tmrk/3ba1cc679e9f655143593524a203b7e2/raw/40436639265c4b85be9c1d886537432b39b71b4d/countries.json';
 
@@ -11,18 +16,22 @@ const FieldOption = (props) => {
   const handleClick = () => {
     field.excluded = !field.excluded;
     setFields([...fields]);
-    console.log('foo');
+    localStorage.setItem('fields', JSON.stringify(fields));
   }
 
   return (
-    <li className={field.excluded ? 'excluded' : ''}>
+    <li 
+      className={field.excluded ? 'excluded' : ''}
+      onPointerDown={handleClick}
+    >
       <span 
         className='checkbox' 
         data-checked='true'
-        onPointerDown={handleClick}
-      ></span>
+      >
+        {field.excluded ? '' : <Checkmark />}
+      </span>
       <span className='key'>"{field.name}"</span>
-      <span className='colon'>: </span> 
+      <span className='colon'>:&nbsp;</span> 
       <span className={'value ' + valueType}>
         {valueType === 'string' ? '"' : ''}
         {countryValue ? countryValue.toString() : 'undefined'}
@@ -34,17 +43,50 @@ const FieldOption = (props) => {
 }
 
 const FieldSelect = (props) => {
-  const {fields, setFields, countries} = props;
-  const country = countries.length && countries[Math.floor(Math.random() * countries.length)];
+  const {countries, fields, setFields, selectedCountry, newSelectedCountry} = props;
 
   let maxFieldLength = 0;
+
   for (let i = 0; i < fields.length; i++) {
     if (maxFieldLength < fields[i].length) maxFieldLength = fields[i].length;
   }
   return (
-    <pre>
+    <div className='fieldselect'>
+      <ul className='header tools'>
+      <li 
+          className='deselect'
+          onPointerDown={() => {
+            for (let i = 0; i < fields.length; i++)  fields[i].excluded = true;
+            setFields([...fields]);
+            localStorage.setItem('fields', JSON.stringify(fields));
+          }}
+        >
+          <Deselect />
+          <span>Deselect all</span>
+        </li>
+
+        <li 
+          className='selectall'
+          onPointerDown={() => {
+            for (let i = 0; i < fields.length; i++)  fields[i].excluded = false;
+            setFields([...fields]);
+            localStorage.setItem('fields', JSON.stringify(fields));
+          }}
+        >
+          <SelectAll />
+          <span>Select all</span>
+        </li>
+
+        <li 
+          className='refresh'
+          onPointerDown={() => newSelectedCountry({random: true})}
+        >
+          <Refresh />
+          <span>Random country</span>
+        </li>
+      </ul>
       <span className='bracket'>&#123;</span>
-      <ul>
+      <ul className='fields'>
         {fields.length ? 
           fields.map((field, index) => 
           <FieldOption 
@@ -52,26 +94,71 @@ const FieldSelect = (props) => {
             field={field}
             fields={fields}
             setFields={setFields}
-            countryValue={country[field.name] ? country[field.name] : ''}
+            countryValue={selectedCountry[field.name] ? selectedCountry[field.name] : ''}
             last={index + 1 === fields.length}
           />
         )
         : ''}
       </ul>
       <span className='bracket'>&#125;</span>
-    </pre>
+      <ul className='footer tools'>
+        <li 
+          className='download'
+          onPointerDown={() => {
+            let a = document.createElement('a');
+            document.body.appendChild(a);
+            a.style = 'display: none';
+            const data = JSON.stringify(countries.map(country => {
+              const newCountry = {};
+              for (let i = 0; i < fields.length; i++) {
+                if (!fields[i].excluded && country[fields[i].name]) {
+                  newCountry[fields[i].name] = country[fields[i].name];
+                }
+              }
+              if (Object.keys(newCountry).length) return newCountry; // needs be fixed to skip null
+            }));
+            let blob = new Blob([data], {type: 'text/plain;charset=utf-8'});
+            let url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = 'countries_' +
+              new Date().toISOString().replace(/[-:]/g, '').replace('T', '_').split('.')[0]
+              + '.json';
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+          }}
+        >
+          <FileDownload />
+          <span>Download in JSON</span>
+        </li>
+      </ul>
+    </div>
   );
 };
+
 
 function App() {
 
   const [countries, setCountries] = useState([]);
-  const [fields, setFields] = useState([]);
+  const [fields, setFields] = useState(
+    //JSON.parse(localStorage.getItem('fields')) || 
+    []
+  );
+  const [selectedCountry, setSelectedCountry] = useState();
+
+  const newSelectedCountry = (props) => {
+    const listOfCountries = props.countries || countries;
+    const newCountry = props.random ?
+      listOfCountries[Math.floor(Math.random() * listOfCountries.length)] :
+      listOfCountries[0];
+    setSelectedCountry(newCountry);
+  };
 
   useEffect(() => {
 
     const processSourceCountries = (sourceCountries) => {
       setCountries(sourceCountries);
+      newSelectedCountry({countries: sourceCountries, random: true});
 
       for (let i = 0; i < sourceCountries.length; i++) {
         const country = sourceCountries[i];
@@ -111,8 +198,14 @@ function App() {
 
   }, []);
 
+
+
+
   return (
-    <div className="App">
+    <div className="container">
+      <header>
+        <h1>CountryList</h1>
+      </header>
       
       {/* <ul>
         {countries.length ?
@@ -121,10 +214,18 @@ function App() {
       </ul> */}
       
       <FieldSelect 
+        countries={countries}
         fields={fields}
         setFields={setFields}
-        countries={countries}
+        selectedCountry={selectedCountry}
+        newSelectedCountry={newSelectedCountry}
       />
+      <footer>
+        <p>
+          &#128073;&nbsp;
+          <a href='https://github.com/tmrk/CountryList'>github.com/tmrk/CountryList</a>
+        </p>
+      </footer>
     </div>
   );
 }
